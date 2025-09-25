50f6ab554909a0781b572eef9acfd90892a7ea9f
Benedikt Wiesner <benedikt.wiesner@brainlab.com>
2025-09-25 08:47:32 +0200

    update

--- backup.sh
+++ backup.sh
@@ -6,7 +6,7 @@ TIMESTAMP=$(date +%F_%H-%M)
 
 mkdir -p $BACKUP_DIR
 
-tar -czvf $BACKUP_DIR/full-backup-$TIMESTAMP.tar.gz \
+tar -czvf $BACKUP_DIR/full-backup-volumes-data-$TIMESTAMP.tar.gz \
   volumes/jenkins \
   volumes/grafana \
   volumes/prometheus \
@@ -15,3 +15,6 @@ tar -czvf $BACKUP_DIR/full-backup-$TIMESTAMP.tar.gz \
   Jenkinsfile \
   prometheus.yml \
   requirements.txt
+
+PARENT_BACKUP="../project-full-backup-$TIMESTAMP.tar.gz"
+tar -czvf "$PARENT_BACKUP" 
