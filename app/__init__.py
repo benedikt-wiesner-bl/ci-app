7a1da18b0667c9baf1812c973e7c8f55aa0a0ae8
Benedikt Wiesner <benedikt.wiesner@brainlab.com>
2025-09-26 08:58:49 +0200

    update

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_FILE = os.path.join(os.path.dirname(BASE_DIR), "data", "todos.db")
 os.makedirs(os.path.dirname(DB_FILE), exist_ok=True)
-#os.makedirs("data", exist_ok=True)
-#DB_FILE = "data/todos.db"
+# os.makedirs("data", exist_ok=True)
+# DB_FILE = "data/todos.db"
 
 
 def get_connection():
